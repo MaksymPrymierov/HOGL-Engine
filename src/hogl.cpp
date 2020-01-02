93450fb97436f04f4d153ef91af703d9d0eda3cc
Vlad Fear <onlylastfear1@gmail.com>
2020-01-03 00:28:06 +0200

    E: Experiments with displaying few triangles
    Add the second triangle with another fragmentShader and
    coordinates.
    
    Signed-off-by: Vlad Fear <onlylastfear1@gmail.com>

--- src/hogl.cpp
+++ src/hogl.cpp
@@ -45,10 +45,28 @@ int main(int argc, char** argv)
 		"	color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 		"}\n";
 
-	unsigned int fragmentShader = 0;
+	const std::string fragmentShaderSrc2 = "#version 330 core\n"
+		"layout (location = 0) out vec4 color;\n"
+		"void main()\n"
+		"{\n"
+		"	color = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+		"}\n";
+
+	unsigned int fragmentShaderO = 0;
 	try
 	{
-		fragmentShader = shaders::createShader(GL_FRAGMENT_SHADER, fragmentShaderSrc);
+		fragmentShaderO = shaders::createShader(GL_FRAGMENT_SHADER, fragmentShaderSrc);
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	unsigned int fragmentShaderY = 0;
+	try
+	{
+		fragmentShaderY = shaders::createShader(GL_FRAGMENT_SHADER, fragmentShaderSrc2);
 	}
 	catch (const std::exception& e)
 	{
@@ -59,37 +77,52 @@ int main(int argc, char** argv)
 	/* Link shaders in shader program */
 	int success = 0;
 	char log[512];
-	unsigned int shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
+	unsigned int shaderProgramOrange = glCreateProgram();
+	glAttachShader(shaderProgramOrange, vertexShader);
+	glAttachShader(shaderProgramOrange, fragmentShaderO);
+	glLinkProgram(shaderProgramOrange);
 
 	/* Check if shader program has been linked without errors */
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	glGetProgramiv(shaderProgramOrange, GL_LINK_STATUS, &success);
 
 	if (!success)
 	{
-		glGetProgramInfoLog(shaderProgram, sizeof(log), nullptr, log);
+		glGetProgramInfoLog(shaderProgramOrange, sizeof(log), nullptr, log);
+		std::cout << "Error during shader program linking\n" << log << std::endl;
+	}
+
+	unsigned int shaderProgramYellow = glCreateProgram();
+	glAttachShader(shaderProgramYellow, vertexShader);
+	glAttachShader(shaderProgramYellow, fragmentShaderY);
+	glLinkProgram(shaderProgramYellow);
+
+	/* Check if shader program has been linked without errors */
+	glGetProgramiv(shaderProgramYellow, GL_LINK_STATUS, &success);
+
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgramYellow, sizeof(log), nullptr, log);
 		std::cout << "Error during shader program linking\n" << log << std::endl;
 	}
 
 	/* Remove shaders */
 	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+	glDeleteShader(fragmentShaderO);
+	glDeleteShader(fragmentShaderY);
 
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f, // Left
-		 0.5f, -0.5f, 0.0f, // Right
-		 0.0f,  0.5f, 0.0f  // Top
+		-1.0f,  0.0f, 0.0f, // Left
+		 0.5f,  0.0f, 0.0f, // Right
+		-0.25f, 1.0f, 0.0f  // Top
 	};
 
-	unsigned int VBO, VAO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
+	unsigned int VBO[2], VAO[2];
+	glGenVertexArrays(2, VAO);
+	glGenBuffers(2, VBO);
 
 	/* Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s). */
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindVertexArray(VAO[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (GLvoid*)0);
@@ -102,6 +135,26 @@ int main(int argc, char** argv)
 	/* Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs) */
 	glBindVertexArray(0);
 
+	float vertices1[] = {
+		-0.5f,  0.0f, 0.0f, // Left
+		 1.0f,  0.0f, 0.0f, // Right
+		 0.25f, 1.0f, 0.0f  // Bot
+	};
+
+	/* Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s). */
+	glBindVertexArray(VAO[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	/* Note that this is allowed, the call to glVertexAttribPointer registered VBO as
+	the currently bound vertex buffer object so afterwards we can safely unbind */
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	/* Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs) */
+	glBindVertexArray(0);
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
@@ -111,16 +164,21 @@ int main(int argc, char** argv)
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
+		glUseProgram(shaderProgramOrange);
+		glBindVertexArray(VAO[0]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+
+		glUseProgram(shaderProgramYellow);
+		glBindVertexArray(VAO[1]);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glBindVertexArray(0);
 		/* Swap front and back buffers */
 		engine.swapBuffers();
 	}
 
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
+	glDeleteVertexArrays(2, VAO);
+	glDeleteBuffers(2, VBO);
 
 	return EXIT_SUCCESS;
 }
